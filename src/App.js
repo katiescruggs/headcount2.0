6186c09a6857328bb9539de72acdedb161ca4580
mariastlouis <msantray@yahoo.com>
2017-11-27 14:23:26 -0700

    Helper js file created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import DistrictRepository from './helper';
+import kinderData from '../data/kindergartners_in_full_day_program';
+
 
 class App extends Component {
+  constructor () {
+    super();
+      this.state = {
+      districtData: new DistrictRepository(kinderData),
+     displayArray: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      displayArray: this.state.districtData
+    });
+  }
+
+
+
   render() {
+    { console.log(this.state.districtData) }
     return (
+      <div className = "App">
       <div>Welcome To Headcount 2.0</div>
+    
+      </div>
     );
   }
 }
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,25 @@
 export default class DistrictRepository {
+  constructor (dirtyData) {
+    this.data = this.dataClean(dirtyData);
+  }
+
+  dataClean(dirtyData) {
+    let cleanData = {};
+
+    cleanData = dirtyData.reduce((dataObject, item) => {
+      const currentDistrict = item.Location.toUpperCase();
+
+      if(!dataObject[currentDistrict]) {
+        dataObject[currentDistrict] = {};
+      }
+      dataObject[currentDistrict][item.TimeFrame] = item;
+       if (typeof dataObject[currentDistrict][item.TimeFrame].Data !== 'number') {
+        dataObject[currentDistrict][item.TimeFrame].Data = 0;
+      }
+      return dataObject;
+    }, {})
+  return cleanData;
+  }
+
 
 }
