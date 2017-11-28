89f120cd09c4f9ae341dcf01a2e5eec1b8c0874e
katiescruggs <katie.e.scruggs@gmail.com>
2017-11-28 15:22:39 -0700

    add filterDistricts function to app, which sets state so that filtered results display

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import DistrictRepository from './helper';
 import kinderData from '../data/kindergartners_in_full_day_program';
-import CardContainer from './CardContainer.js'
+import CardContainer from './CardContainer.js';
+import Search from './Search.js';
 
 class App extends Component {
   constructor () {
@@ -17,18 +18,21 @@ class App extends Component {
     this.setState({
       displayArray: this.state.districtData.findAllMatches()
     }, () => console.log(this.state.displayArray) );
-
   }
 
-
+  filterDistricts = (searchTerm) => {
+    const filteredDistricts = this.state.districtData.findAllMatches(searchTerm);
+    console.log(filteredDistricts);
+    this.setState({displayArray: filteredDistricts})
+  }
 
   render() {
-  if(this.state.displayArray.length > 100) {
+  if(this.state.displayArray.length) {
     return (
       <div className="App">
-      <div>Welcome To Headcount 2.0</div>
-      <CardContainer
-      districtArray={this.state.displayArray} />
+        <h1>Welcome To Headcount 2.0</h1>
+        <Search filterDistricts={this.filterDistricts}/>
+        <CardContainer districtArray={this.state.displayArray} />
       </div>
     );
   } else {
