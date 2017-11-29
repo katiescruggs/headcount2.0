bcaa3557fab6ba2d22cfd5229990f44f428c6a6e
mariastlouis <msantray@yahoo.com>
2017-11-28 21:41:27 -0700

    cards resized and styled with a background color in the head

--- src/Card.js
+++ src/Card.js
@@ -8,14 +8,14 @@ const checkIcon = (num) => {
   return num >= 0.5 ? <Check /> : null;
 }
 
-//
+
 
 function displayData(districtData) {
   if (districtData !== undefined) {
   const years = Object.keys(districtData);
     return years.map((year, index) => {
         return (
-         <li key={index}> <span className = "icon-span">{checkIcon(districtData[year])} </span> <strong>{year}</strong>: {districtData[year]}</li>
+         <li key={index}> <span className="icon-span">{checkIcon(districtData[year])} </span> <strong>{year}</strong>: {districtData[year]}</li>
         )
       })
   } else {
