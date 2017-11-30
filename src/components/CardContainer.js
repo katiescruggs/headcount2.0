df9709f9a473b5db318d626a00c8458f07ba151b
mariastlouis <msantray@yahoo.com>
2017-11-30 16:48:31 -0700

    change to the way state displays

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -11,23 +11,21 @@ const CardContainer = ({districtArray, comparisonCards, handleClick}) => (
           let type = '';
           let buttonText = 'compare';
 
-          if(comparisonCards.length === 1) {
-            if(district.location === comparisonCards[0].location) {
-              type = 'card-clicked';
-              buttonText = 'remove';
-            }
-          } else if(comparisonCards.length === 2) {
-            if(district.location === comparisonCards[0].location || district.location === comparisonCards[1].location) {
-              type = 'card-clicked';
-              buttonText = 'remove';
-            }
-          }
+          // if(comparisonCards.length === 1) {
+          //   if(district.location === comparisonCards[0].location) {
+          //     type = 'card-clicked';
+          //     buttonText = 'remove';
+          //   }
+          // } else if(comparisonCards.length === 2) {
+          //   if(district.location === comparisonCards[0].location || district.location === comparisonCards[1].location) {
+          //     type = 'card-clicked';
+          //     buttonText = 'remove';
+          //   }
+          // }
           return (
             <Card 
               key={district.location}
               id={district.location}
-              type={type}
-              buttonText = {buttonText}
               districtName={district.location}  
               districtData={district.data}
               handleClick = {handleClick} />
