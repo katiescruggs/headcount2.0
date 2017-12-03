791a11b1347c1f44edf5d0bc3c1b2238e141b455
mariastlouis <msantray@yahoo.com>
2017-12-03 14:29:50 -0700

    Header tests for select component inside

--- test/Header.test.js
+++ test/Header.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import DataSetSelect from '../src/components/DataSetSelect.js';
 import Header from '../src/components/Header.js'
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 describe ('Header', () => {
   it ('has one select element', () => {
-    const select = shallow(<Header />)
+     const expectedState = {
+      dataFileNames: ['Full Day Kindergartners', 'High School Grad Rates', 'Student Enrollment'], 
+      currentDataFile: 'Student Enrollment'            
+    };
+    const mockFunc = jest.fn()
+
+    const select = mount(<Header
+      dataFileNames={expectedState.dataFileNames}
+      currentDataFile={expectedState.currentDataFile}
+      changeDataSet = {mockFunc} />)
     expect(select.find('select').length).toEqual(1);
     console.log(select.debug())
   })
+
+  
 
 
 })
