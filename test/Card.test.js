ac4234e41e80b683aca58c6f9f9bbf5865a3d0d4
katiescruggs <katie.e.scruggs@gmail.com>
2017-12-03 12:59:19 -0700

    alphabetize css rules

--- test/Card.test.js
+++ test/Card.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme'
 import Card from '../src/components/Card.js'
 
 describe ('Card Tests', () => {
-    it('has a card class name an h2 and a button', () => {
+  it('has a card class name an h2 and a button', () => {
     const district = {location: 'Academy 20', data: {2014: 1, 2015: 1} }
     const mockHandleClick = jest.fn()
     const wrapper = shallow ( <Card 
@@ -19,10 +19,10 @@ describe ('Card Tests', () => {
     expect(wrapper.find('h2').length).toEqual(1);
   });
 
-    it('should render a checkmark next to the data if the value is greater than 0.5', () =>{
-      const district = {location: 'Colorado', data: {2014: 0.3, 2015: 1, 2016: 0.6} }
-      const mockHandleClick = jest.fn()
-      const wrapper = shallow ( <Card 
+  it('should render a checkmark next to the data if the value is greater than 0.5', () =>{
+    const district = {location: 'Colorado', data: {2014: 0.3, 2015: 1, 2016: 0.6} }
+    const mockHandleClick = jest.fn()
+    const wrapper = shallow ( <Card 
             key={district.location}
             id={district.location}
             districtName={district.location}
@@ -30,13 +30,13 @@ describe ('Card Tests', () => {
             handleClick = {mockHandleClick}
             type= ''
             buttonText='Compare' /> );
-      expect(wrapper.find('FaCheck').length).toEqual(2);
-    });
+    expect(wrapper.find('FaCheck').length).toEqual(2);
+  });
 
-    it('should call the handleClick function if the compare button is clicked', () => {
-      const district = {location: 'Colorado', data: {2014: 0.3, 2015: 1, 2016: 0.6} }
-      const mockHandleClick = jest.fn()
-      const wrapper = shallow ( <Card 
+  it('should call the handleClick function if the compare button is clicked', () => {
+    const district = {location: 'Colorado', data: {2014: 0.3, 2015: 1, 2016: 0.6} }
+    const mockHandleClick = jest.fn()
+    const wrapper = shallow ( <Card 
             key={district.location}
             id={district.location}
             districtName={district.location}
@@ -45,9 +45,9 @@ describe ('Card Tests', () => {
             type= ''
             buttonText='Compare' /> );
 
-      expect(mockHandleClick.mock.calls.length).toEqual(0);
+    expect(mockHandleClick.mock.calls.length).toEqual(0);
 
-      wrapper.find('button').simulate('click');
-      expect(mockHandleClick.mock.calls.length).toEqual(1);
-    });
+    wrapper.find('button').simulate('click');
+    expect(mockHandleClick.mock.calls.length).toEqual(1);
+  });
 });
