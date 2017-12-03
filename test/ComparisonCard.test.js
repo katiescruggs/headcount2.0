b6583b05334e119983a5807c11105d7f12cb9e6b
mariastlouis <msantray@yahoo.com>
2017-12-03 15:46:58 -0700

    Added snapshot test to comparsion card tests

--- test/ComparisonCard.test.js
+++ test/ComparisonCard.test.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-
 import ComparisonCard from '../src/components/ComparisonCard.js';
+import toJson from 'enzyme-to-json'
+
 
 describe('ComparisonCard', () => {
-  it('should have one h2, three spans, and six p tags', () => {
-    const mockCompareAverages = (districtOne, districtTwo) => {
-      return {[districtOne]: 'district one average', 
-              [districtTwo]: 'district two average',
-              compared: 'comparison ratio'  
-            };
+  let wrapper;
+  let mockCompareAverages = (districtOne, districtTwo) => {
+    return {[districtOne]: 'district one average', 
+            [districtTwo]: 'district two average',
+            compared: 'comparison ratio'  
     };
-    const wrapper = shallow(<ComparisonCard districtOne='COLORADO' districtTwo='ACADEMY 20' compareDistrictAverages={mockCompareAverages}/>)
-    
+  };
+  beforeEach(() =>{
+    wrapper = shallow(<ComparisonCard districtOne='COLORADO' districtTwo='ACADEMY 20' compareDistrictAverages={mockCompareAverages}/>)
+  });
+
+  it('should match the snapshot', () =>{
+    expect(toJson(wrapper)).toMatchSnapshot()
+  });
+
+  it('should have one h2 and six p tags', () => {
     expect(wrapper.find('h2').length).toEqual(1);
-    expect(wrapper.find('span').length).toEqual(3);
     expect(wrapper.find('p').length).toEqual(6);
   });
 
   it('should display districtOne, districtTwo, and the comparison between them', () => {
-    const mockCompareAverages = (districtOne, districtTwo) => {
-      return {[districtOne]: 'district one average', 
-              [districtTwo]: 'district two average',
-              compared: 'comparison ratio'  
-            };
-    };
-    const wrapper = shallow(<ComparisonCard districtOne='COLORADO' districtTwo='ACADEMY 20' compareDistrictAverages={mockCompareAverages}/>)
-    expect(wrapper.find('.district1-avg').text()).toEqual(' district one average ');
-    expect(wrapper.find('.district2-avg').text()).toEqual(' district two average ');
-    expect(wrapper.find('.comparison').text()).toEqual(' comparison ratio ');
+    expect(wrapper.find('.district1-avg').text()).toEqual('district one average');
+    expect(wrapper.find('.district2-avg').text()).toEqual('district two average');
+    expect(wrapper.find('.comparison').text()).toEqual('comparison ratio');
   });
 });
