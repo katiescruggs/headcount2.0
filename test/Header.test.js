59b4c1a2bc6ac9a6d4626a6831e95db6a3ac42e7
mariastlouis <msantray@yahoo.com>
2017-12-03 14:55:35 -0700

    On change file test complete

--- test/Header.test.js
+++ test/Header.test.js
@@ -4,22 +4,31 @@ import Header from '../src/components/Header.js'
 import { shallow, mount } from 'enzyme';
 
 describe ('Header', () => {
-  it ('has one select element', () => {
-     const expectedState = {
-      dataFileNames: ['Full Day Kindergartners', 'High School Grad Rates', 'Student Enrollment'], 
-      currentDataFile: 'Student Enrollment'            
-    };
-    const mockFunc = jest.fn()
+  let header;
+  let expectedState ={
+    dataFileNames: ['Full Day Kindergartners', 'High School Grad Rates', 'Student Enrollment'], 
+    currentDataFile: 'Student Enrollment' 
+  };
+  const mockFunc = jest.fn()
 
-    const select = mount(<Header
+  beforeEach(() => {
+    header = mount(<Header
       dataFileNames={expectedState.dataFileNames}
-      currentDataFile={expectedState.currentDataFile}
+      CurrentDataFile={expectedState.currentDataFile}
       changeDataSet = {mockFunc} />)
-    expect(select.find('select').length).toEqual(1);
-    console.log(select.debug())
+
+  });
+
+  it ('has one select element', () => {
+    expect(header.find('select').length).toEqual(1);
+  });
+
+  it('runs changeDataSet on change', () =>{
+    header.find('select').simulate('change', {target: {value: 'High School Grad Rates'}})
+    expect(mockFunc.mock.calls.length).toEqual(1);
   })
 
-  
+
 
 
 })
